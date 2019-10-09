66697ac8954b82fc518e7b71dfe6f523c3ff67b3
Pedrolo <pedrolopez7411@gmail.com>
2019-10-09 18:28:53 -0300

    modificaciones en database

--- database/db_verduleria.sql
+++ database/db_verduleria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-09-2019 a las 01:23:38
+-- Tiempo de generación: 09-10-2019 a las 23:11:24
 -- Versión del servidor: 10.1.40-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -25,13 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `idAdmin` int(11) NOT NULL,
+  `adminName` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `admins`
+--
+
+INSERT INTO `admins` (`idAdmin`, `adminName`, `password`) VALUES
+(2, 'locasso', '$2y$10$VxzuU0Lutp47kQCp7CG9Z.7sFw9SZqGBz1LX96J7PoERvwhy9Du1K');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `categorias`
 --
 
 CREATE TABLE `categorias` (
-  `idCategoria` int(11) NOT NULL,
-  `Nombre` int(11) NOT NULL
+  `idCategoria` int(25) NOT NULL,
+  `Nombre` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`idCategoria`, `Nombre`) VALUES
+(1, 'Fruta'),
+(2, 'Verdura');
 
 -- --------------------------------------------------------
 
@@ -44,19 +71,26 @@ CREATE TABLE `productos` (
   `Nombre` varchar(25) NOT NULL,
   `Precio` int(50) NOT NULL,
   `Descripcion` varchar(144) NOT NULL,
-  `Categoria` int(25) NOT NULL
+  `idCategoria` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`idProducto`, `Nombre`, `Precio`, `Descripcion`, `Categoria`) VALUES
-(1, 'Pomelo', 55, 'Citrico', 0);
+INSERT INTO `productos` (`idProducto`, `Nombre`, `Precio`, `Descripcion`, `idCategoria`) VALUES
+(1, 'Pomelo', 55, 'Citrico', 1),
+(2, 'Mandarina', 25, 'Citrico', 1);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`idAdmin`);
 
 --
 -- Indices de la tabla `categorias`
@@ -68,23 +102,40 @@ ALTER TABLE `categorias`
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`idProducto`);
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `FK_idCategoria` (`idCategoria`) USING BTREE;
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategoria` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
